e3d4e18808ccd0c32d16279aff50ee5bf935a215
mukeshkanna11 <agriculturistmukesh11@gmail.com>
2024-08-25 23:40:09 +0530

    Day 02 Updated

---
+++ Day-02/Day2 Readme.me
@@ -0,0 +1,42 @@
+//DAY-02
+
+1. Alerts & Prompts?
+  
+  These methods are provide by the browser intreact with user for getting input.
+
+  ALert: It is display a message to user with OK button.
+  Prompt: It's display a dialog box for ask input from user
+
+2.  Operators- Arithmetic & Logical?
+
+Arithmetic Operators: 
+ Arithmetic operators perform mathematical calculations on numbers.
+ ● +(Addition): Adds two numbers.
+ ●-(Subtraction): Subtracts the second number from the first.
+ ● *(Multiplication): Multiplies two numbers.
+ ● /(Division): Divides the first number by the second.
+ ● %(Modulus): Returns the remainder of a division.
+ ● **(Exponentiation): Raises the first number to the power of the second.
+
+Logical Opeerators:
+ Logical operators are used to perform logical operations, usually in conditions (like if
+ statements).
+ ● &&(Logical AND): Returns true if both operands are true.
+ ● ||(Logical OR): Returns true if at least one of the operands is true.
+ ● !(Logical NOT): Inverts the value of a boolean expression
+
+ 3. Variables & Data Types ?
+
+ Variables:
+ Variables in JavaScript are used to store data(any type) that can be referenced and manipulated
+ later in the code.
+ ● var:Declares a variable (function-scoped / Global Scope).
+ ● let:Declares a block-scoped variable.(local)
+ ● const:Declares a block-scoped constant (cannot be reassigned)
+
+What is ES in JavaScript?
+
+ ES stands for ECMAScript, which is the standardized scripting language
+ that JavaScript is based on. ECMAScript is maintained by ECMA
+ International through the TC39 committee, and it provides the
+ specifications that JavaScript implementations follow.
---
+++ Day-02/day2 script.js
@@ -0,0 +1,84 @@
+// DAY-02!!
+
+// 1.Alerts & Prompts:
+
+// alert("User Name Is Found") // its has only OK Button.
+
+// let names=prompt ("Enter the age") // sting value
+// console.log(names); // It's display browser console and it's shows OK & CANCEL button.
+
+// let a=number(prompt ("Enter the value")) // number value
+// console.log(a); 
+
+// 2. Operators- Arithmetic & Logical
+
+     // Arithmetic Operators:
+          
+    //  let b=12
+    //  let c=4
+    //  console.log(b+c);
+    //  console.log(b-c);
+    //  console.log(b*c);
+    //  console.log(b/c);
+    //  console.log(b%c);
+    //  console.log(b**c);
+
+     // Logical Operators:
+
+    //  let d=true
+    //  let e=false
+    //  console.log(d&&e);    
+    //  console.log(d||e);    
+    //  console.log(d!=e);    
+
+
+     //and:    // && syntax od And // Output true means both values are true
+
+// let d=true
+// let e=false
+// console.log(c&&d);
+
+// let d=true
+// let e=true
+// console.log(e&&d);
+
+// let x=false
+// let y=false
+// console.log(y&&x);
+
+//or :    // || syntax od OR // Output true means atleast values are true
+ 
+// let d= false
+// let e= false
+// console.log(e||d);
+
+// let d= false
+// let e= true
+// console.log(e||d);
+
+// let d= true
+// let e= true
+// console.log(e||d);
+
+//not :   // != syntax od NOT // totally opposite of OR
+
+
+// let d= true
+// let e= true
+// console.log(d!=e);
+
+// let d= true
+// let e= false
+// console.log(e!=d);
+
+
+//3.Variables and Datatypes?
+
+// var name="Mukesh"         // var define Global Scope
+// console.log(name);
+
+// let age=23                // let define Local Scope
+// console.log(age);
+
+// const state="Tamilnadu"    // const define cannot reassigned
+// console.log(state);
